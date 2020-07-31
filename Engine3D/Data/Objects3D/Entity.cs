311bdd7fcdfd7c29bc1b1f41b0a6c0fc24c9e178
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2020-07-31 22:44:13 +0200

    Integrating CI Added Position to Entity

--- Engine3D/Data/Objects3D/Entity.cs
+++ Engine3D/Data/Objects3D/Entity.cs
@@ -3,22 +3,42 @@ using System.Collections.Generic;
 using Engine3D.Data.Objects3D.Mesh;
 using Engine3D.Data.Shaders;
 using Engine3D.Data.Textures;
+using OpenTK;
 
 namespace Engine3D.Data.Objects3D
 {
     public class Entity : IEntity
     {
+	    private bool disposed;
+
 		private Mesh3D mesh;
 		private List<Texture> textures;
+		private Vector3 entityPos;
 
-		public Entity(Mesh3D mesh, Texture[] textures)
+		public Entity(Mesh3D mesh, Texture[] textures,Vector3 entityPos)
 		{
 			this.mesh = mesh;
 			this.textures = new List<Texture>(textures);
+			this.entityPos = entityPos;
 		}
 		public void Dispose()
 		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
+		public void Dispose(bool disposing)
+		{
+			if (!disposing) return;
+			if (!disposed)
+			{
+				mesh.Dispose();
+				for (int i = 0; i < textures.Count; i++)
+				{
+					textures[i].Dispose();
+				}
+				disposed = true;
+			}
 		}
 		public void Draw(IShader shader)
 		{
@@ -28,7 +48,8 @@ namespace Engine3D.Data.Objects3D
 
 		public void Draw()
 		{
-			throw new NotImplementedException();
+			// TODO apply textures
+			mesh.Draw();
 		}
     }
 }
