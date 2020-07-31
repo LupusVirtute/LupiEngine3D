5079510f8a59b65c2890511a0e63daa730d0c456
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2020-07-31 22:10:01 +0200

    Added a scene rendering

---
+++ Engine3D/Data/World/IScene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Engine3D.Data.Objects3D;
+using Engine3D.Data.Shaders;
+
+namespace Engine3D.Data.World
+{
+    public interface IScene : IDisposable
+    {
+	    bool AddEntityToScene(IEntity entity);
+	    bool RemoveEntityByReference(IEntity entity);
+	    void Render(IShader shader);
+	    void Render();
+    }
+}
---
+++ Engine3D/Data/World/Scene.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Engine3D.Data.Objects3D;
+using Engine3D.Data.Shaders;
+
+namespace Engine3D.Data.World
+{
+    public class Scene : IScene
+    {
+	    private bool disposed;
+
+	    private List<IEntity> entities;
+
+	    public void Dispose()
+	    {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+	    }
+		/// <summary>
+		/// Adds entity to rendering
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <returns></returns>
+	    public bool AddEntityToScene(IEntity entity)
+	    {
+		    if (entity == null)
+			    return false;
+			entities.Add(entity);
+			return true;
+	    }
+
+		public bool RemoveEntityByReference(IEntity entity)
+		{
+			if (entity == null)
+				return false;
+			entities.Remove(entity);
+			return true;
+		}
+
+	    public void Dispose(bool disposing)
+	    {
+		    if (!disposed)
+		    {
+			    if (disposing)
+			    {
+				    for (int i = 0; i < entities.Count; i++)
+				    {
+					    entities[i].Dispose();
+				    }
+				    disposed = true;
+			    }
+		    }
+	    }
+		public void Render(IShader shader)
+	    {
+			shader.Use();
+			Render();
+	    }
+
+	    public void Render()
+	    {
+		    for (int i = 0; i < entities.Count; i++)
+		    {
+			    entities[i].Draw();
+		    }
+	    }
+    }
+}
