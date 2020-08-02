345261f3432f11a590f5333b920eeb99e8442997
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2020-08-02 18:54:37 +0200

    fs

--- Engine3D/Data/Objects3D/Entity.cs
+++ Engine3D/Data/Objects3D/Entity.cs
@@ -3,42 +3,22 @@ using System.Collections.Generic;
 using Engine3D.Data.Objects3D.Mesh;
 using Engine3D.Data.Shaders;
 using Engine3D.Data.Textures;
-using OpenTK;
 
 namespace Engine3D.Data.Objects3D
 {
     public class Entity : IEntity
     {
-	    private bool disposed;
-
 		private Mesh3D mesh;
 		private List<Texture> textures;
-		private Vector3 entityPos;
 
-		public Entity(Mesh3D mesh, Texture[] textures,Vector3 entityPos)
+		public Entity(Mesh3D mesh, Texture[] textures)
 		{
 			this.mesh = mesh;
 			this.textures = new List<Texture>(textures);
-			this.entityPos = entityPos;
 		}
 		public void Dispose()
 		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
 
-		public void Dispose(bool disposing)
-		{
-			if (!disposing) return;
-			if (!disposed)
-			{
-				mesh.Dispose();
-				for (int i = 0; i < textures.Count; i++)
-				{
-					textures[i].Dispose();
-				}
-				disposed = true;
-			}
 		}
 		public void Draw(IShader shader)
 		{
@@ -48,8 +28,7 @@ namespace Engine3D.Data.Objects3D
 
 		public void Draw()
 		{
-			// TODO apply textures
-			mesh.Draw();
+			throw new NotImplementedException();
 		}
     }
 }
--- Engine3D/Data/Vertex.cs
+++ Engine3D/Data/Vertex.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using OpenTK;
+﻿using OpenTK;
 
 namespace Engine3D.Data
 {
