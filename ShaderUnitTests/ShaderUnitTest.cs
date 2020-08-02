034e0c17d3e7ea30a3902dafff133b3f6da01716
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2020-08-02 20:23:26 +0200

    Works?

--- ShaderUnitTests/ShaderUnitTest.cs
+++ ShaderUnitTests/ShaderUnitTest.cs
@@ -1,44 +1,43 @@
 ﻿using System.IO;
 using Engine3D.Data.Shaders;
-using NUnit.Framework;
 using Engine3D.Files;
-using Assert = NUnit.Framework.Assert;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShaderUnitTests
 {
 	namespace ShaderUnitTest
 	{
+		[TestClass]
 		public class ShaderConstructTests
 		{
 			private string _verShaderPath;
 			private string _fragShaderPath;
-			[SetUp]
+			[TestInitialize]
 			public void SetUp()
 			{
 				_verShaderPath = FileDataManagment.MainVerShaderPath;
 				_fragShaderPath = FileDataManagment.MainFragShaderPath;
 			}
-			[Test]
+			[TestMethod]
 			public void ShaderConstructor_VerShaderMissing_FileNotFoundException()
 			{
-				Assert.Throws<FileNotFoundException>(
+				Assert.ThrowsException<FileNotFoundException>(
 					() => new Shader("", "")
 				);
 			}
-			[Test]
+			[TestMethod]
 			public void ShaderConstructor_FragShaderMissing_FileNotFoundException()
 			{
 
-
-				Assert.Throws<FileNotFoundException>(
+				Assert.ThrowsException<FileNotFoundException>(
 					() => new Shader(_verShaderPath, "")
 				);
 			}
-			[Test]
+			[TestMethod]
 			public void ShaderConstructor_Success_ShaderCreated()
 			{
-				Assert.DoesNotThrow(
-					() => new Shader(_verShaderPath, _fragShaderPath)
+				NUnit.Framework.Assert.DoesNotThrow(
+				() => new Shader(_verShaderPath, _fragShaderPath)
 				);
 			}
 
